c705068e72ba62844373ec14d6f60f9ec75e4c77
Christopher <cthall83@gmail.com>
2020-06-27 22:02:16 -0700

    Auto stash before rebase of "origin/master"

--- dicerolls.py
+++ dicerolls.py
@@ -1,18 +1,65 @@
 import random
+import argparse
 
-def dice(num,max=6):
-    '''Rolls num(ber) of max(imum) sided dice'''
+
+def dice(num, var=6):
+    '''Rolls a num(ber) of var(ible) sided dice'''
+    if num % 1 != 0:
+        print('I\'m not rolling a fractional die for anyone.')
+        quit()
+    if var % 1 != 0:
+        print('Seriously, I can\'t roll dice with fractional sides; what would that even mean?')
+        quit()
+    if num == 0:
+        print('So you don\'t want to roll any dice.')
+        quit()
+    if num < 0:
+        print('Negative numbers, huh? \nOkay, you roll the dice then. Let me know how it goes.')
+        quit()
+    if var < 1:
+        print ("I see you're unfamiliar with dice. Dice must have at least one side.")
+        quit()
+
+    if num == 1 and var == 1:
+        print('What\'s the point of rolling a 1-sided die?')
+    elif num == 1 and var == 2:
+        print('So you\'re flipping a coin.')
+    elif var == 1:
+        print("What's the point of rolling 1-sided dice?")
+    elif var == 2:
+        print('Let the coin flipping begin!')
+
+    num = int(num)
+    var = int(var)
+
     rolls = list()
     for each in range(num):
         # print(each)
-        rolls.append(random.randint(1,max))
+        rolls.append(random.randint(1, var))
     return rolls
 
-def dice_string(num,max=6):
+
+def dice_string(num, var=6):
     '''Creates a string of dice rolls useful for dicewords'''
     rolls = ''
     for each in range(num):
         if (each != 0 and each % 5 == 0):
             rolls += ':'
-        rolls += str(random.randint(1,max))
+        rolls += str(random.randint(1, var))
     return rolls
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="""
+    This script rolls a number of variable sided dice. 
+    """)
+    parser.add_argument("number", help="Number of dice", type=float)
+    parser.add_argument("--sides", help="Amount of sides of dice", default=6, type=float)
+
+    args = parser.parse_args()
+
+    number = args.number
+    sides = args.sides
+
+    result=dice(number, sides)
+    print(result)
--- xkcd_password.py
+++ xkcd_password.py
@@ -1,8 +1,37 @@
 import secrets
+import argparse
 
-def xkcd_style_password(word_amount = 4, max_char = 25):
-    with open('/usr/share/dict/words') as f:
-        words = [word.strip() for word in f if len(word.strip()) <= max_char]
-        password = ' '.join(secrets.choice(words) for i in range(word_amount))
+
+def xkcd_style_password(word_amount=4, min_char=1, max_char=24):
+    if max_char < min_char:
+        print(
+            'Maximum character value ({max}) cannot be less than minimum character value ({min}).'.format(max=max_char,
+                                                                                                          min=min_char)
+        )
+        quit()
+    try:
+        with open('/usr/share/dict/words') as f:
+            words = [word.strip() for word in f if (len(word.strip()) <= max_char and len(word.strip()) >= min_char)]
+            password = ' '.join(secrets.choice(words) for i in range(word_amount))
         return password
+    except IndexError:
+        print('No words in that range of min/max characters.')
+        quit()
 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="""
+    This script rolls a number of variable sided dice. 
+    """)
+    parser.add_argument("--words", help="Number of words", default=4, type=int)
+    parser.add_argument("--min_char", help="Minimum amount of characters per word", default=1, type=int)
+    parser.add_argument("--max_char", help="Maximum amount of characters per word", default=24, type=int)
+
+    args = parser.parse_args()
+
+    number = args.words
+    min_char = args.min_char
+    max_char = args.max_char
+
+    result = xkcd_style_password(number, min_char, max_char)
+    print(result)
